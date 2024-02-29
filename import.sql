962129110ab037431f3a4eef4f41f44d26779c66
Bob Hodges <bobhodges1@outlook.com>
2024-02-29 11:45:55 +0000

    foreign keys added

--- import.sql
+++ import.sql
@@ -14,3 +14,20 @@ CREATE TABLE transaction_type (
   INSERT INTO transaction_type (type_name)
   SELECT DISTINCT type
   FROM transactions;
+
+
+-- created type_id as column as a foreign key, and dropped type from transactions. There is now a link between transactions and transaction_type
+
+ALTER TABLE transactions
+ADD COLUMN type_id INT,
+ADD CONSTRAINT fk_transaction_type
+FOREIGN KEY (type_id)
+REFERENCES transaction_type(type_id);
+
+UPDATE transactions AS t
+SET type_id = tt.type_id
+FROM transaction_type AS tt
+WHERE t.type = tt.type_name;
+
+ALTER TABLE transactions
+DROP COLUMN type;
